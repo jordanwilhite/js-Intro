874a590c3e542c02b7034d7342731be8c6eb5435
Jordan Wilhite <jordanwilhite@gmail.com>
2015-07-09 17:08:17 -0500

    This is my completed homework

---
+++ main.js
@@ -0,0 +1,49 @@
+var givenName;
+// Q: What does `givenName` equal right now?
+// A: undefined;
+
+givenName = "Tim";
+// Q: What is `givenName` set to right now?
+// Tim;
+
+givenName = "JD";
+// Q: What is `givenName` set to right now?
+
+
+var greeting = "Hello, how are you?";
+// Q: What is `greeting` set to?
+//  "Hello, how are you?";
+
+
+var high = 50;
+var low  = 10;
+
+
+var math = high - low;
+// Q: What is `math` set to?
+// A: math is set to high-low;
+// A: Answer returns as '40';
+
+
+math = high - "5";
+// Q: What is `math` set to?
+// A: math is set to high - "5";
+// A: Answer returns as 45
+
+
+// Create a variable to calculate Tim's age
+// The answer should read "Tim is 31 years old"
+var born = 1983;
+var today = 2015;
+// var age = today - born;
+// var result('Tim is' + age + "years old");
+
+
+// Store some information following in variables.
+var yourName; Jordan;
+var instructorName; Will;
+
+var statement = yourName + " is taking a class at The Iron Yard, my instructor's name is " + instructorName;
+// 'Jordan is taking a class at The Iron Yard, my instructors name is Will';
+
+//about to add to git
